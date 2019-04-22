33c0a29ecc3e5f83a61b8e6ae9d6f992c9338a36
Marcos Freire <marcosdefontes@gmail.com>
2019-04-22 11:23:13 -0300

    Alternativo config for rollup

---
+++ build/rollup.config.2.js
@@ -0,0 +1,60 @@
+
+import path from 'path';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import vue from 'rollup-plugin-vue';
+
+const extensions = [
+  '.js', '.jsx', '.ts', '.tsx',
+];
+
+const resolve = _path => path.resolve(__dirname, '../', _path)
+const basename = 'tueble';
+
+export default [{
+  file: resolve('dist/tueble.js'),
+  format: 'umd',
+  env: 'development',
+  name: 'Tueble',
+  exports: 'named',
+}].map(genConfig);
+
+
+function genConfig(opts) {
+  const config = {
+    input: resolve('src/entry.js'),
+    plugins: [
+
+      commonjs(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      vue({
+        css: true,
+        compileTemplate: true,
+        template: {
+          isProduction: true,
+        },
+      }),
+    ],
+    output:{
+      file: opts.file,
+      format: 'umd',
+      name: 'Tueble',
+      exports: 'named'
+    },
+  }
+
+  if (opts.env) {
+    config.plugins.unshift(replace({
+      'process.env.NODE_ENV': JSON.stringify(opts.env)
+    }))
+  }
+
+  if (opts.transpile !== false) {
+    babel({ extensions, include: ['src/**//*'] })
+  }
+
+  return config
+}
